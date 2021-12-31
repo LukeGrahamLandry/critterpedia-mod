19ab2ba3d6c9f1c864a4d2c0f96c648f3518a720
Luke Graham Landry <40009893+LukeGrahamLandry@users.noreply.github.com>
2021-12-31 16:13:33 -0500

    Bat Wings: mirror and rest positions

--- src/main/java/ca/lukegrahamlandry/critterpedia/content/client/models/ModdedBatModel.java
+++ src/main/java/ca/lukegrahamlandry/critterpedia/content/client/models/ModdedBatModel.java
@@ -67,21 +67,43 @@ public class ModdedBatModel extends EntityModel<ModdedBatEntity> {
 
         float speed = 1.0F;
         float degree = 1.0F;
-        this.RightWing.zRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * 5.0F) * f1 * 0.5F;
-        this.RightWingTwo.yRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * 5.0F) * f1 * 0.5F;
-        this.RightWing.yRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * 1.0F) * f1 * 0.5F;
-        this.LeftWing.zRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -5.0F) * f1 * 0.5F;
-        this.LeftWingTwo.yRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -5.0F) * f1 * 0.5F;
-        this.LeftWing.yRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -1.0F) * f1 * 0.5F;
-        this.Body.y = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * 1.0F) * f1 * 0.5F;
-        this.Body.xRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * 1.0F) * f1 * 0.5F;
-        this.Body.xRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -1.0F) * f1 * 0.5F;
-        if (this.Head != null){
-            this.Head.y = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -0.2F) * f1 * 0.5F;
-            this.Head.xRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -0.2F) * f1 * 0.5F;
-        }
-        if (this.Legs != null){
-            this.Legs.xRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -1.0F) * f1 * 0.5F;
+
+
+        if (entity.isResting()){
+            boolean isWeird = entity.getType().getRegistryName().getPath().equals("hoary_bat") || entity.getType().getRegistryName().getPath().equals("big_eared_bat");
+            this.RightWing.zRot = - (degree * 5.0F) * f1 * 0.5F;
+
+            this.RightWingTwo.yRot = - (degree * 5.0F) * f1 * 0.5F;
+
+            this.RightWing.yRot = - (degree * 1.0F) * f1 * 0.5F;
+            this.LeftWing.zRot = - (degree * -5.0F) * f1 * 0.5F;
+            this.LeftWingTwo.yRot = - (degree * -5.0F) * f1 * 0.5F;
+            this.LeftWing.yRot = - (degree * -1.0F) * f1 * 0.5F;
+
+            if (isWeird) {
+                this.RightWingTwo.yRot *= 0;
+                this.LeftWingTwo.yRot *= 0;
+                this.RightWingTwo.zRot = -1;
+                this.LeftWingTwo.zRot = 1;
+            }
+
+        } else {
+            this.RightWing.zRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * 5.0F) * f1 * 0.5F;
+            this.RightWingTwo.yRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * 5.0F) * f1 * 0.5F;
+            this.RightWing.yRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * 1.0F) * f1 * 0.5F;
+            this.LeftWing.zRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -5.0F) * f1 * 0.5F;
+            this.LeftWingTwo.yRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -5.0F) * f1 * 0.5F;
+            this.LeftWing.yRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -1.0F) * f1 * 0.5F;
+            this.Body.y = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * 1.0F) * f1 * 0.5F;
+            this.Body.xRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * 1.0F) * f1 * 0.5F;
+            this.Body.xRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -1.0F) * f1 * 0.5F;
+            if (this.Head != null){
+                this.Head.y = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -0.2F) * f1 * 0.5F;
+                this.Head.xRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -0.2F) * f1 * 0.5F;
+            }
+            if (this.Legs != null){
+                this.Legs.xRot = Mth.cos((f * speed * 0.2F) + (float) Math.PI) * (degree * -1.0F) * f1 * 0.5F;
+            }
         }
     }
 
@@ -96,11 +118,11 @@ public class ModdedBatModel extends EntityModel<ModdedBatEntity> {
                     PartDefinition root = partdefinition.addOrReplaceChild("root", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
                     PartDefinition Body = root.addOrReplaceChild("Body", CubeListBuilder.create().texOffs(25, 0).addBox(-3.5F, -2.0F, 0.0F, 7.0F, 4.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 22.0F, -5.0F));
                     PartDefinition LeftWing = Body.addOrReplaceChild("LeftWing", CubeListBuilder.create().texOffs(0, 11).mirror().addBox(-8.0F, -1.0F, 0.0F, 8.0F, 15.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-3.0F, 0.5F, 1.0F, 1.5708F, 0.0F, 0.0F));
-                    PartDefinition LeftWingTwo = LeftWing.addOrReplaceChild("LeftWingTwo", CubeListBuilder.create().texOffs(21, 11).mirror().addBox(-13.0F, -1.0F, 0.0F, 13.0F, 13.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-8.0F, 0.0F, 0.0F));
+                    PartDefinition LeftWingTwo = LeftWing.addOrReplaceChild("LeftWingTwo", CubeListBuilder.create().texOffs(21, 11).addBox(-13.0F, -1.0F, 0.0F, 13.0F, 13.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-8.0F, 0.0F, 0.0F));
                     PartDefinition Body2 = Body.addOrReplaceChild("Body2", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -2.0F, 0.0F, 6.0F, 4.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 6.0F));
                     PartDefinition Legs = Body.addOrReplaceChild("Legs", CubeListBuilder.create().texOffs(0, 50).addBox(-3.0F, -0.5F, 0.0F, 6.0F, 1.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 1.0F, 5.0F));
                     PartDefinition RightWing = Body.addOrReplaceChild("RightWing", CubeListBuilder.create().texOffs(0, 11).addBox(0.0F, -1.0F, 0.0F, 8.0F, 15.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(3.0F, 0.5F, 1.0F, 1.5708F, 0.0F, 0.0F));
-                    PartDefinition RightWingTwo = RightWing.addOrReplaceChild("RightWingTwo", CubeListBuilder.create().texOffs(21, 11).addBox(0.0F, -1.0F, 0.0F, 13.0F, 13.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(8.0F, 0.0F, 0.0F));
+                    PartDefinition RightWingTwo = RightWing.addOrReplaceChild("RightWingTwo", CubeListBuilder.create().texOffs(21, 11).mirror().addBox(0.0F, -1.0F, 0.0F, 13.0F, 13.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(8.0F, 0.0F, 0.0F));
                     PartDefinition Head = Body.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(0, 30).addBox(-3.0F, -2.5F, -4.0F, 6.0F, 5.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
                     PartDefinition Snout = Head.addOrReplaceChild("Snout", CubeListBuilder.create().texOffs(30, 30).addBox(-1.5F, -1.5F, 0.0F, 3.0F, 3.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 1.0F, -7.0F));
                     PartDefinition Ears = Head.addOrReplaceChild("Ears", CubeListBuilder.create().texOffs(23, 45).addBox(-2.0F, -1.0F, 0.0F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -2.5F, -2.0F));
@@ -115,9 +137,9 @@ public class ModdedBatModel extends EntityModel<ModdedBatEntity> {
                     PartDefinition root = partdefinition.addOrReplaceChild("root", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
                     PartDefinition Body = root.addOrReplaceChild("Body", CubeListBuilder.create().texOffs(0, 0).addBox(-2.5F, -2.0F, 0.0F, 5.0F, 4.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 22.0F, -1.0F));
                     PartDefinition LeftWing = Body.addOrReplaceChild("LeftWing", CubeListBuilder.create().texOffs(0, 11).mirror().addBox(-5.0F, -1.0F, 0.0F, 5.0F, 10.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-2.5F, 0.5F, 0.0F, 1.5708F, 0.0F, 0.0F));
-                    PartDefinition LeftWingTwo = LeftWing.addOrReplaceChild("LeftWingTwo", CubeListBuilder.create().texOffs(13, 11).mirror().addBox(-9.0F, -1.0F, 0.0F, 9.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-5.0F, 0.0F, 0.0F));
+                    PartDefinition LeftWingTwo = LeftWing.addOrReplaceChild("LeftWingTwo", CubeListBuilder.create().texOffs(13, 11).addBox(-9.0F, -1.0F, 0.0F, 9.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-5.0F, 0.0F, 0.0F));
                     PartDefinition RightWing = Body.addOrReplaceChild("RightWing", CubeListBuilder.create().texOffs(0, 11).addBox(0.0F, -1.0F, 0.0F, 5.0F, 10.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.5F, 0.5F, 0.0F, 1.5708F, 0.0F, 0.0F));
-                    PartDefinition RightWingTwo = RightWing.addOrReplaceChild("RightWingTwo", CubeListBuilder.create().texOffs(13, 11).addBox(0.0F, -1.0F, 0.0F, 9.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(5.0F, 0.0F, 0.0F));
+                    PartDefinition RightWingTwo = RightWing.addOrReplaceChild("RightWingTwo", CubeListBuilder.create().texOffs(13, 11).mirror().addBox(0.0F, -1.0F, 0.0F, 9.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(5.0F, 0.0F, 0.0F));
                     PartDefinition Legs = Body.addOrReplaceChild("Legs", CubeListBuilder.create().texOffs(3, 26).addBox(-2.5F, -0.5F, 0.0F, 5.0F, 1.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.99F, 6.0F));
                     PartDefinition Head = Body.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(28, 2).addBox(-2.5F, -2.0F, -4.0F, 5.0F, 4.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-0.01F, 0.0F, 0.0F));
                     PartDefinition Ear1 = Head.addOrReplaceChild("Ear1", CubeListBuilder.create().texOffs(31, 19).addBox(-1.0F, 0.0F, 0.0F, 1.0F, 3.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(-2.5F, -2.0F, -3.0F));
@@ -134,9 +156,9 @@ public class ModdedBatModel extends EntityModel<ModdedBatEntity> {
                     PartDefinition root = partdefinition.addOrReplaceChild("root", CubeListBuilder.create(), PartPose.offset(0.0F, 0.0F, 0.0F));
                     PartDefinition Body = root.addOrReplaceChild("Body", CubeListBuilder.create().texOffs(0, 0).addBox(-2.5F, -2.0F, 0.0F, 5.0F, 4.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 22.0F, -1.0F));
                     PartDefinition LeftWing = Body.addOrReplaceChild("LeftWing", CubeListBuilder.create().texOffs(0, 11).mirror().addBox(-5.0F, -1.0F, 0.0F, 5.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(-2.5F, 0.5F, 0.0F, 1.5708F, 0.0F, 0.0F));
-                    PartDefinition LeftWingTwo = LeftWing.addOrReplaceChild("LeftWingTwo", CubeListBuilder.create().texOffs(14, 11).mirror().addBox(-8.0F, -1.0F, 0.0F, 8.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-5.0F, 0.0F, 0.0F));
+                    PartDefinition LeftWingTwo = LeftWing.addOrReplaceChild("LeftWingTwo", CubeListBuilder.create().texOffs(14, 11).addBox(-8.0F, -1.0F, 0.0F, 8.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-5.0F, 0.0F, 0.0F));
                     PartDefinition RightWing = Body.addOrReplaceChild("RightWing", CubeListBuilder.create().texOffs(0, 11).addBox(0.0F, -1.0F, 0.0F, 5.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(2.5F, 0.5F, 0.0F, 1.5708F, 0.0F, 0.0F));
-                    PartDefinition RightWingTwo = RightWing.addOrReplaceChild("RightWingTwo", CubeListBuilder.create().texOffs(14, 11).addBox(0.0F, -1.0F, 0.0F, 8.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(5.0F, 0.0F, 0.0F));
+                    PartDefinition RightWingTwo = RightWing.addOrReplaceChild("RightWingTwo", CubeListBuilder.create().texOffs(14, 11).mirror().addBox(0.0F, -1.0F, 0.0F, 8.0F, 8.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(5.0F, 0.0F, 0.0F));
                     PartDefinition Legs = Body.addOrReplaceChild("Legs", CubeListBuilder.create().texOffs(18, 0).addBox(-3.5F, -0.5F, 0.0F, 7.0F, 1.0F, 3.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.99F, 6.0F));
                     PartDefinition Head = Body.addOrReplaceChild("Head", CubeListBuilder.create().texOffs(30, 15).addBox(-2.0F, -2.0F, -7.0F, 4.0F, 4.0F, 7.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
                     PartDefinition Ears = Head.addOrReplaceChild("Ears", CubeListBuilder.create().texOffs(39, 3).addBox(-2.0F, 0.0F, 0.0F, 4.0F, 1.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -3.0F, -2.0F));
@@ -154,9 +176,9 @@ public class ModdedBatModel extends EntityModel<ModdedBatEntity> {
                     PartDefinition tail = body.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(10, 15).addBox(-2.5F, 0.0F, 0.0F, 5.0F, 0.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 1.0F, 4.5F));
                     PartDefinition nose = body.addOrReplaceChild("nose", CubeListBuilder.create().texOffs(19, 24).addBox(-1.5F, -1.0F, -1.0F, 3.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, -3.5F));
                     PartDefinition wing1Left = body.addOrReplaceChild("LeftWing", CubeListBuilder.create().texOffs(-7, 11).mirror().addBox(-6.0F, 0.0F, -4.0F, 6.0F, 0.0F, 8.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-2.5F, 1.0F, 2.5F));
-                    PartDefinition wing2Left = wing1Left.addOrReplaceChild("LeftWingTwo", CubeListBuilder.create().texOffs(-7, 2).mirror().addBox(-6.0F, 0.0F, -1.0F, 6.0F, 0.0F, 8.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-6.0F, 0.0F, -3.0F));
+                    PartDefinition wing2Left = wing1Left.addOrReplaceChild("LeftWingTwo", CubeListBuilder.create().texOffs(-7, 2).addBox(-6.0F, 0.0F, -1.0F, 6.0F, 0.0F, 8.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-6.0F, 0.0F, -3.0F));
                     PartDefinition wing1Right = body.addOrReplaceChild("RightWing", CubeListBuilder.create().texOffs(-7, 11).addBox(0.0F, 0.0F, -4.0F, 6.0F, 0.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(2.5F, 1.0F, 2.5F));
-                    PartDefinition wing2Right = wing1Right.addOrReplaceChild("RightWingTwo", CubeListBuilder.create().texOffs(-7, 2).addBox(0.0F, 0.0F, -1.0F, 6.0F, 0.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(6.0F, 0.0F, -3.0F));
+                    PartDefinition wing2Right = wing1Right.addOrReplaceChild("RightWingTwo", CubeListBuilder.create().texOffs(-7, 2).mirror().addBox(0.0F, 0.0F, -1.0F, 6.0F, 0.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(6.0F, 0.0F, -3.0F));
                     PartDefinition earRight = body.addOrReplaceChild("earRight", CubeListBuilder.create().texOffs(1, 21).addBox(0.0F, -3.0F, -1.0F, 1.0F, 4.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(2.5F, -1.0F, -2.5F));
                     PartDefinition earLeft = body.addOrReplaceChild("earLeft", CubeListBuilder.create().texOffs(1, 21).mirror().addBox(-1.0F, -3.0F, -1.0F, 1.0F, 4.0F, 2.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-2.5F, -1.0F, -2.5F));
 
@@ -173,11 +195,11 @@ public class ModdedBatModel extends EntityModel<ModdedBatEntity> {
                     PartDefinition earLeft = body.addOrReplaceChild("earLeft", CubeListBuilder.create().texOffs(0, 13).addBox(-1.0F, -6.0F, -0.5F, 3.0F, 7.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-2.5F, -1.0F, -2.0F, 0.0F, 0.0F, -0.2618F));
                     PartDefinition earRight = body.addOrReplaceChild("earRight", CubeListBuilder.create().texOffs(0, 13).mirror().addBox(-2.0F, -6.0F, -0.5F, 3.0F, 7.0F, 1.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(2.5F, -1.0F, -2.0F, 0.0F, 0.0F, 0.2618F));
                     PartDefinition wing1Right = body.addOrReplaceChild("RightWing", CubeListBuilder.create().texOffs(1, 13).addBox(0.0F, 0.0F, -4.0F, 7.0F, 0.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(2.5F, 1.0F, 0.5F));
-                    PartDefinition wing2Right = wing1Right.addOrReplaceChild("RightWingTwo", CubeListBuilder.create().texOffs(-8, 4).addBox(0.0F, 0.0F, -1.0F, 6.0F, 0.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(7.0F, 0.0F, -3.0F));
+                    PartDefinition wing2Right = wing1Right.addOrReplaceChild("RightWingTwo", CubeListBuilder.create().texOffs(-8, 4).mirror().addBox(0.0F, 0.0F, -1.0F, 6.0F, 0.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(7.0F, 0.0F, -3.0F));
                     PartDefinition tail = body.addOrReplaceChild("tail", CubeListBuilder.create().texOffs(9, 8).addBox(-2.5F, 0.0F, 0.0F, 5.0F, 0.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 1.0F, 2.5F));
                     PartDefinition nose = body.addOrReplaceChild("nose", CubeListBuilder.create().texOffs(17, 24).addBox(-1.5F, -1.0F, -1.0F, 3.0F, 2.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 1.0F, -3.5F));
                     PartDefinition wing1Left = body.addOrReplaceChild("LeftWing", CubeListBuilder.create().texOffs(1, 13).mirror().addBox(-7.0F, 0.0F, -4.0F, 7.0F, 0.0F, 8.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-2.5F, 1.0F, 0.5F));
-                    PartDefinition wing2Left = wing1Left.addOrReplaceChild("LeftWingTwo", CubeListBuilder.create().texOffs(-8, 4).mirror().addBox(-6.0F, 0.0F, -1.0F, 6.0F, 0.0F, 8.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-7.0F, 0.0F, -3.0F));
+                    PartDefinition wing2Left = wing1Left.addOrReplaceChild("LeftWingTwo", CubeListBuilder.create().texOffs(-8, 4).addBox(-6.0F, 0.0F, -1.0F, 6.0F, 0.0F, 8.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-7.0F, 0.0F, -3.0F));
 
                     return LayerDefinition.create(meshdefinition, 32, 32);
                 };
--- src/main/java/ca/lukegrahamlandry/critterpedia/content/client/render/ModdedBatRender.java
+++ src/main/java/ca/lukegrahamlandry/critterpedia/content/client/render/ModdedBatRender.java
@@ -3,6 +3,10 @@ package ca.lukegrahamlandry.critterpedia.content.client.render;
 import ca.lukegrahamlandry.critterpedia.ModMain;
 import ca.lukegrahamlandry.critterpedia.content.client.models.ModdedBatModel;
 import ca.lukegrahamlandry.critterpedia.content.entity.ModdedBatEntity;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Matrix4f;
+import com.mojang.math.Vector3f;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
@@ -14,6 +18,15 @@ public class ModdedBatRender extends MobRenderer<ModdedBatEntity, ModdedBatModel
 
     public ModdedBatRender(EntityRendererProvider.Context ctx, Function<EntityRendererProvider.Context, ModdedBatModel> model) {
         super(ctx, model.apply(ctx), 0.3F);
+    }
+
+    @Override
+    protected void setupRotations(ModdedBatEntity bat, PoseStack stack, float p_115319_, float p_115320_, float p_115321_) {
+        super.setupRotations(bat, stack, p_115319_, p_115320_, p_115321_);
+        if (bat.isResting()){
+            stack.mulPose(Vector3f.XN.rotationDegrees(180));
+            stack.translate(0, -1, 0);
+        }
     }
 
     public ResourceLocation getTextureLocation(ModdedBatEntity bat) {
